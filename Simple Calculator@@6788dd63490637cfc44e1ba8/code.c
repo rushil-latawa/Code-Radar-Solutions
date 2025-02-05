787367e3eec075678c8a54550a7b667fcc9addc3
rushil-latawa <rushillatawa@gmail.com>
2025-02-05 12:42:02 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 int main(){
-    int a , b,sum;
+    int a , b,sum,d,e,f;
     char c;
     scanf("%d %d %c",&a,&b,&c);
     sum= a+b;
+    d= a-b;
+    e=a*b;
+    f=a/b;
     if (c=='+'){
         printf("%d",sum);
+    }
+    else if (c=='-'){
+        printf("%d",d);
+    }
+    else if (c=='*'){
+        printf("%d",e);
+    }
+    else if (c=='/'){
+        printf("%d",f);
     }
 }    
